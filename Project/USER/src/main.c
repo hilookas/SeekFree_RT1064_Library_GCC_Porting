5501433f340582a35a59b124de35083e24e69bd2
lookas <i@18kas.com>
2021-05-19 16:02:16 +0800

    Add RT-Thread version link

--- Project/USER/src/main.c
+++ Project/USER/src/main.c
@@ -1,13 +1,13 @@
 #include "include.h"
 
-int main(void) {
+void main(void) {
   DisableGlobalIRQ(); // 关闭总中断
   board_init();       // 初始化 MPU 时钟 调试串口
 
   gpio_init(B9, GPO, 0, GPIO_PIN_CONFIG); // 初始化 B9 端口输出模式 初始化之后输出低电平 使用默认引脚配置 GPIO_PIN_CONFIG
 
   EnableGlobalIRQ(0); // 开启总中断
-  while(1) {
+  while (1) {
     gpio_set(B9, 1);       // 熄灭核心板 LED
     systick_delay_ms(100); // 延时 100ms
     gpio_set(B9, 0);       // 点亮核心板 LED
@@ -15,7 +15,7 @@ int main(void) {
   }
 }
 
-// int main(void) {
+// void main(void) {
 //   DisableGlobalIRQ();
 //   board_init();
 
